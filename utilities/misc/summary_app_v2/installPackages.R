b3d7c26f5aae58fee8d524e8026c8118bf224deb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 14:12:42 +0800

    Delete utilities directory

--- utilities/misc/summary_app_v2/installPackages.R
+++
@@ -1,9 +0,0 @@
-# Ashish Kulshrestha | kulshresthaa@pbworld.com | Parsons Brinckerhoff
-# Last Edited: Feb 5, 2018
-# Script to check if requred packages are already installed, if not then install the packages.
-
-packages <- c("shinyjs", "xlsx", "plotly", "readr", "dplyr", "tidyr")
-
-if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())), repos = "https://cran.cnr.berkeley.edu/")  
-}
--- utilities/misc/summary_app_v2/run.R
+++
@@ -1,8 +0,0 @@
-args=commandArgs(trailingOnly=TRUE)
-folder_address = args[1]
-
-if(!"shiny" %in% installed.packages()) install.packages("shiny", repos = "https://cran.cnr.berkeley.edu/")
-if(!"shinydashboard" %in% installed.packages()) install.packages("shinydashboard", repos = "https://cran.cnr.berkeley.edu/")
-
-suppressWarnings(suppressMessages(require(shiny)))
-suppressWarnings(suppressMessages(runApp(folder_address, launch.browser=TRUE)))
